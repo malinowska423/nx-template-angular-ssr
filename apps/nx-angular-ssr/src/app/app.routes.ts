a3f9efe649783b737e3c3a200ece0aa833336665
Aleksandra Malinowska <44405328+malinowska423@users.noreply.github.com>
2025-04-26 19:46:46 +0200

    Add the Layout library (#1)

--- apps/nx-angular-ssr/src/app/app.routes.ts
+++ apps/nx-angular-ssr/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () => import('@nx-template-angular-ssr/layout').then((r) => r.LAYOUT_ROUTES),
+  },
+];
---
+++ libs/layout/src/lib/layout.routes.ts
@@ -0,0 +1,8 @@
+import { LayoutComponent } from './layout/layout.component';
+
+export const LAYOUT_ROUTES = [
+  {
+    path: '',
+    component: LayoutComponent,
+  },
+];
