dd218d1a493ecafb56cebf83adde53802c96334b
malinowska423 <amalinowska423@gmail.com>
2025-04-26 19:36:52 +0200

    Add the Layout library

---
+++ libs/layout/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/layout.routes';
+export * from './lib/layout/layout.component';
---
+++ libs/layout/src/lib/layout/layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-layout',
+  imports: [],
+  template: ` <p>Layout works!</p> `,
+})
+export class LayoutComponent {}
